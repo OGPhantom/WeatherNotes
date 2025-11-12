a5a35c9912b3e5ed397c856bdb82368a24985c20
OGPhantom <nikita.storchai@nure.ua>
2025-11-12 22:32:48 +0200

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Никита Сторчай on 12.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
